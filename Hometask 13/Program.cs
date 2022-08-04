f76ec54d7e959c8c91985f4bf7e65dd12a9b9cd1
Dmitrii Romanenco <dmitrii.romanenco@amdaris.com>
2022-08-04 23:29:33 +0300

    Add project files.

---
+++ Hometask 13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+// Task 1
+//     Sync two dirs, FIRST & SECOND
+//     - if a file exist in FIRST but not in SECOND, copy
+//     - if a file exists in FIRST and in SECOND, but content
+//     changed, then FIRST should overwrite the one from SECOND
+//     - if a file exists in SECOND but not in FIRST, it should be removed
+
+// Task 2
+//     Sync two dirs, FIRST & SECOND in real time
+//     - listen to the changes in FIRST and sync the actions with SECOND 
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
