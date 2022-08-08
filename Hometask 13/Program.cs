2fd8d4776897d851c2a3160e9c582c09bbbc6b35
Dmitrii Romanenco <dmitrii.romanenco@amdaris.com>
2022-08-08 16:55:20 +0300

    new

--- Hometask 13/Program.cs
+++ Hometask 13/Program.cs
@@ -17,76 +17,11 @@ namespace App
             string destinationPath = @"C:\Users\dmitrii.romanenco\Downloads\Hometask 13\SECOND";
 
 
-            FileManager fm = new (sourcePath, destinationPath);
+            FileManager fm = new(sourcePath, destinationPath);
 
             fm.DirectorySynchronization();
-
-
-
         }
 
-
-        public static void CopyFolderContents(string sourceFolder, string destinationFolder, string mask, Boolean createFolders, Boolean recurseFolders)
-        {
-            try
-            {
-                var exDir = sourceFolder;
-                var dir = new DirectoryInfo(exDir);
-                var destDir = new DirectoryInfo(destinationFolder);
-
-                SearchOption so = (recurseFolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
-
-                foreach (string sourceFile in Directory.GetFiles(dir.ToString(), mask, so))
-                {
-                    FileInfo srcFile = new FileInfo(sourceFile);
-                    string srcFileName = srcFile.Name;
-
-                    // Create a destination that matches the source structure
-                    FileInfo destFile = new FileInfo(destinationFolder + srcFile.FullName.Replace(sourceFolder, ""));
-
-                    if (!Directory.Exists(destFile.DirectoryName) && createFolders)
-                    {
-                        Directory.CreateDirectory(destFile.DirectoryName);
-                    }
-
-                    //Check if src file was modified and modify the destination file
-                    if (srcFile.LastWriteTime > destFile.LastWriteTime || !destFile.Exists)
-                    {
-                        File.Copy(srcFile.FullName, destFile.FullName, true);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                //Debug.WriteLine(ex.Message + Environment.NewLine + Environment.NewLine + ex.StackTrace);
-            }
-        }
-
-        private static void DeleteAll(DirectoryInfo source, DirectoryInfo target)
-        {
-            if (!source.Exists)
-            {
-                target.Delete(true);
-                return;
-            }
-
-            // Delete each existing file in target directory not existing in the source directory.
-            foreach (FileInfo fi in target.GetFiles())
-            {
-                var sourceFile = Path.Combine(source.FullName, fi.Name);
-                if (!File.Exists(sourceFile)) //Source file doesn't exist, delete target file
-                {
-                    fi.Delete();
-                }
-            }
-
-            // Delete non existing files in each subdirectory using recursion.
-            foreach (DirectoryInfo diTargetSubDir in target.GetDirectories())
-            {
-                DirectoryInfo nextSourceSubDir = new DirectoryInfo(Path.Combine(source.FullName, diTargetSubDir.Name));
-                DeleteAll(nextSourceSubDir, diTargetSubDir);
-            }
-        }
     }
 
 
@@ -418,7 +353,7 @@ namespace App
 //            }
 //        }
 
-//        public bool IsFile (string path)
+//        public bool IsFile(string path)
 //        {
 //            try
 //            {
